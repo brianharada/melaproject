b52384ff8c1b77c5dcdb706659654ffacdf4426c
Travis Barber <tbarber@melaleuca.com>
2014-08-20 11:20:58 -0600

    add property to gulp-sass for graceful error messages

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -301,7 +301,7 @@ function createGulpFile() {
           "\t\t\trelative: false\n" +
           "\t\t}))\n";
       } else {
-        code += "\t\t.pipe(sass({ style: 'expanded' }))\n";
+        code += "\t\t.pipe(sass({ style: 'expanded', errLogToConsole: true }))\n";
         code += "\t\t.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))\n";
       }      
       if (pathobj.networkpath){
